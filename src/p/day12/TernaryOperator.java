a9a6d1ce3cd1db8783da057e67de182d18b23bcf
Medya Deva <medyadeva@gmail.com>
2022-05-18 12:10:38 +0300

    second commit

--- src/p/day12/TernaryOperator.java
+++ src/p/day12/TernaryOperator.java
@@ -6,16 +6,16 @@ import static java.lang.System.in;
 
 public class TernaryOperator {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(in);
-        System.out.println("Enter temperature");
-        int temperature = scanner.nextInt();
-        String className=temperature<15 ? "Cold weather:" : "Good";
-        System.out.println(className);
-
-
-        int income=75_000;
-        String weather=income>100_000 ? "First Class": "Economy Class";
-        System.out.println(weather);
+//        Scanner scanner = new Scanner(in);
+//        System.out.println("Enter temperature");
+//        int temperature = scanner.nextInt();
+//        String className=temperature<15 ? "Cold weather:" : "Good";
+//        System.out.println(className);
+//
+//
+//        int income=75_000;
+//        String weather=income>100_000 ? "First Class": "Economy Class";
+//        System.out.println(weather);
 
 
 
